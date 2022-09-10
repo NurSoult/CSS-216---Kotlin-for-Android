f12c4d9bbe89880cd2fb235384231160648535e6
nursen <200103098@stu.sdu.edu.kz>
2022-09-10 21:53:39 +0600

    3. Learn more about functions (step 2 final)

--- src/main/kotlin/Main.kt
+++ src/main/kotlin/Main.kt
@@ -1,6 +1,11 @@
 import java.util.*    // required import
 
-fun fishFood (day : String ) : String {
+fun randomDay() : String {
+    val week = arrayOf("Monday", "Tuesday", "Wednesday", "Thursday", "Friday", "Saturday", "Sunday")
+    return week[Random().nextInt(week.size)]
+}
+
+fun fishFood (day : String) : String {
     return when (day) {
         "Monday" -> "flakes"
         "Wednesday" -> "redworms"
@@ -14,7 +19,9 @@ fun fishFood (day : String ) : String {
 fun feedTheFish() {
     val day = randomDay()
     val food = fishFood(day)
-
     println("Today is $day and the fish eat $food")
 }
 
+fun main (args : Array<String>) {
+    feedTheFish()
+}
