264b11448f953202851f76556d76072e53231701
nursen <200103098@stu.sdu.edu.kz>
2022-09-11 16:25:08 +0600

    5. Get started with filters (step 2 1-4)

--- src/main/kotlin/Main.kt
+++ src/main/kotlin/Main.kt
@@ -3,5 +3,13 @@ import java.util.*    // required import
 val decorations = listOf("rock", "pagoda", "plastic plant", "alligator", "flowerpot")
 
 fun main() {
-    println(decorations.filter { it[0] == 'p' })
+    val eager = decorations.filter { it[0] == 'p' }
+    println("eager: $eager")
+
+    val filtered = decorations.asSequence().filter { it[0] == 'p' }
+    println("filtered: $filtered")
+
+    val newList = filtered.toList()
+    println("new list: $newList")
 }
+
