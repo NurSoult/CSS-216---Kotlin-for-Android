96796336fd643f6e9a924d7abda04bf2accd42f5
nursen <200103098@stu.sdu.edu.kz>
2022-09-11 16:07:09 +0600

    5. Get started with filters (step 1)

--- src/main/kotlin/Main.kt
+++ src/main/kotlin/Main.kt
@@ -1,48 +1,7 @@
 import java.util.*    // required import
 
-fun fishFood (day : String) : String {
-    return when (day) {
-        "Monday" -> "flakes"
-        "Wednesday" -> "redworms"
-        "Thursday" -> "granules"
-        "Friday" -> "mosquitoes"
-        "Sunday" -> "plankton"
-        else -> "nothing"
-    }
+val decorations = listOf("rock", "pagoda", "plastic plant", "alligator", "flowerpot")
+
+fun main() {
+    println(decorations.filter { it[0] == 'p' })
 }
-
-fun feedTheFish() {
-    val day = randomDay()
-    val food = fishFood(day)
-    println("Today is $day and the fish eat $food")
-    println("Change water: ${shouldChangeWater(day)}")
-}
-
-fun main(args: Array<String>) {
-    feedTheFish()
-}
-
-fun randomDay() : String {
-    val week = arrayOf ("Monday", "Tuesday", "Wednesday", "Thursday", "Friday", "Saturday", "Sunday")
-    return week[Random().nextInt(week.size)]
-}
-
-fun swim(speed: String = "fast") {
-    println("swimming $speed")
-}
-
-fun shouldChangeWater (day: String, temperature: Int = 22, dirty: Int = 20): Boolean {
-    return when {
-        isTooHot(temperature) -> true
-        isDirty(dirty) -> true
-        isSunday(day) -> true
-        else -> false
-    }
-}
-
-fun isTooHot(temperature: Int) = temperature > 30
-
-fun isDirty(dirty: Int) = dirty > 30
-
-fun isSunday(day: String) = day == "Sunday"
-
