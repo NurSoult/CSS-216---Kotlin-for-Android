c7fbccc7e9717dde562a10ed7972850e0dc963b1
nursen <200103098@stu.sdu.edu.kz>
2022-09-11 15:24:19 +0600

    4. Explore default values and compact functions (step 1)

--- src/main/kotlin/Main.kt
+++ src/main/kotlin/Main.kt
@@ -1,27 +1,11 @@
 import java.util.*    // required import
 
-fun randomDay() : String {
-    val week = arrayOf("Monday", "Tuesday", "Wednesday", "Thursday", "Friday", "Saturday", "Sunday")
-    return week[Random().nextInt(week.size)]
+fun swim(speed : String = "fast") {
+    println("swimming $speed")
 }
 
-fun fishFood (day : String) : String {
-    return when (day) {
-        "Monday" -> "flakes"
-        "Wednesday" -> "redworms"
-        "Thursday" -> "granules"
-        "Friday" -> "mosquitoes"
-        "Sunday" -> "plankton"
-        else -> "nothing"
-    }
-}
-
-fun feedTheFish() {
-    val day = randomDay()
-    val food = fishFood(day)
-    println("Today is $day and the fish eat $food")
-}
-
-fun main (args : Array<String>) {
-    feedTheFish()
+fun main(args : Array<String>) {
+    swim()
+    swim("slow")
+    swim(speed = "turtle-like")
 }
