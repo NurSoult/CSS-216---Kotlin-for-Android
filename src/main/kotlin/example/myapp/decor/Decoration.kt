4c3d90ec6b818ad302f763402cea1788d76e28aa
nursen <200103098@stu.sdu.edu.kz>
2022-09-18 18:21:36 +0600

    9. Learn about singletons and enums (Step 2)

--- src/main/kotlin/example/myapp/decor/Decoration.kt
+++ src/main/kotlin/example/myapp/decor/Decoration.kt
@@ -3,6 +3,14 @@ package example.myapp.decor
 data class Decoration2(val rocks: String, val wood: String, val diver: String) {
 }
 
+enum class Color(val rgb: Int) {
+    RED(0xFF0000), GREEN(0x00FF00), BLUE(0x0000FF);
+}
+
+enum class Direction(val degrees: Int) {
+    NORTH(0), SOUTH(180), EAST(90), WEST(270)
+}
+
 fun makeDecorations() {
     val d5 = Decoration2("crystal", "wood", "diver")
     println(d5)
@@ -14,5 +22,7 @@ fun makeDecorations() {
 }
 
 fun main() {
-    makeDecorations()
+    println(Direction.EAST.name)
+    println(Direction.EAST.ordinal)
+    println(Direction.EAST.degrees)
 }
