067933b286d189a4d7fed849bfc6d63e1616966e
nursen <200103098@stu.sdu.edu.kz>
2022-09-18 13:33:28 +0600

    4. Learn about visibility modifiers

--- src/main/kotlin/example/myapp/Aquarium.kt
+++ src/main/kotlin/example/myapp/Aquarium.kt
@@ -17,7 +17,7 @@ class Aquarium(var length: Int = 100, var width: Int = 20, var height: Int = 40)
     }
     var volume: Int
         get() = width * height * length / 1000
-        set(value) {
+        private set(value) {
             height = (value * 1000) / (width * length)
         }
 }
