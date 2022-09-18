ce032f2c9769c2b4773086bd23557a9c863841ad
nursen <200103098@stu.sdu.edu.kz>
2022-09-18 16:12:24 +0600

    5. Learn about subclasses and inheritance (Step 2)

--- src/main/kotlin/example/myapp/Aquarium.kt
+++ src/main/kotlin/example/myapp/Aquarium.kt
@@ -1,5 +1,7 @@
 package example.myapp
 
+import kotlin.math.PI
+
 open class Aquarium(open var length: Int = 100, open var width: Int = 20, open var height: Int = 40) {
     init {
         println("aquarium initializing")
@@ -24,5 +26,16 @@ open class Aquarium(open var length: Int = 100, open var width: Int = 20, open v
     open val shape = "rectangle"
     open var water: Double = 0.0
         get() = volume * 0.9
+}
 
+class TowerTank (override var height: Int, var diameter: Int): Aquarium(height = height, width = diameter, length = diameter) {
+    override var volume: Int
+
+    get() = (width/2 * length/2 * height / 1000 * PI).toInt()
+    set(value) {
+        height = ((value * 1000 / PI) / (width/2 * length/2)).toInt()
+    }
+
+    override var water = volume * 0.8
+    override var shape = "cylinder"
 }
