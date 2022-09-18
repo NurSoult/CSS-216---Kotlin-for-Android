a5f47ac214e4ef56d410057a81f9dfd470b4dc9e
nursen <200103098@stu.sdu.edu.kz>
2022-09-18 14:51:33 +0600

    5. Learn about subclasses and inheritance (Step 1)

--- src/main/kotlin/example/myapp/Aquarium.kt
+++ src/main/kotlin/example/myapp/Aquarium.kt
@@ -1,23 +1,28 @@
 package example.myapp
 
-class Aquarium(var length: Int = 100, var width: Int = 20, var height: Int = 40) {
+open class Aquarium(open var length: Int = 100, open var width: Int = 20, open var height: Int = 40) {
     init {
         println("aquarium initializing")
     }
 
     fun printSize() {
+        println(shape)
         println("Width: $width cm " +
                 "Length: $length cm " +
                 "Height: $height cm")
-        println("Volume: $volume liters")
+        println("Volume: $volume liters Water: $water liters (${water / volume * 100.0}% full)")
     }
     constructor(numberOfFish: Int) : this() {
         val tank = numberOfFish * 2000 * 1.1
         height = (tank / (length * width)).toInt()
     }
-    var volume: Int
+    open var volume: Int
         get() = width * height * length / 1000
-        private set(value) {
+        set(value) {
             height = (value * 1000) / (width * length)
         }
+    open val shape = "rectangle"
+    open var water: Double = 0.0
+        get() = volume * 0.9
+
 }
