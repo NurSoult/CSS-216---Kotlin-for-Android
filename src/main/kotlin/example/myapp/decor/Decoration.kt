021577d520765b4a68e652e30c6a8dce00aee26f
nursen <200103098@stu.sdu.edu.kz>
2022-09-19 15:09:53 +0600

    2. Learn about pairs and triples (Step 1 3)

--- src/main/kotlin/example/myapp/decor/Decoration.kt
+++ src/main/kotlin/example/myapp/decor/Decoration.kt
@@ -2,6 +2,7 @@ package example.myapp.decor
 
 
 fun main() {
-    val equipment = "fish net" to "catching fish"
-    println("${equipment.first} used for ${equipment.second}")
+    val numbers = Triple(6, 9, 42)
+    println(numbers.toString())
+    println(numbers.toList())
 }
